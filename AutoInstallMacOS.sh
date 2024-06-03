91d29bb1f9435be9e2ca0a18a1379d931d0a4a73
philipnickel <tipkik@hotmail.com>
2024-06-03 13:16:56 +0200

    echo vspath

--- AutoInstallMacOS.sh
+++ AutoInstallMacOS.sh
@@ -82,6 +82,7 @@ clear
 echo "Installing Visual Studio Code if not already installed"
 # if output is empty, then install vs code
 vspath=$(/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dtudk/pythonsupport-scripts/main/multipleVersionsMac.sh)")
+echo "$vspath"
 if [ -z "$vspath" ]; then
     echo "Visual Studio Code is already installed"
 else
