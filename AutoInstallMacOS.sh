7975d824d71874cf91b13189b43e6403f7ffda58
philipnickel <tipkik@hotmail.com>
2024-07-25 11:19:39 +0200

    Made some changes regarding error check ect.

--- AutoInstallMacOS.sh
+++ AutoInstallMacOS.sh
@@ -1,43 +1,56 @@
 # Error function 
 # Print error message, contact information and exits script
-
 exit_message () {
     echo "Oh no! Something went wrong"
-    echo "Please try to install manually or contact the Python Support Team: pythonsupport@dtu.dk or visit us during our office hours"
+    echo "Please try to install manually or contact the Python Support Team:" 
+    echo "Pythonsupport@dtu.dk"
+    echo "Or visit us during our office hours"
+    exit 1
 }
-
 
 # Script installs miniconda and vs code 
 # Welcome text 
-
 echo "Welcome to the MacOS Auto Installer Script"
 echo "This script will install miniconda and Visual Studio Code on your MacOS"
 echo "Please don't close the terminal until the installation is complete"
 echo "This might take a while depending on your internet connection and what dependencies needs to be installed"
-# Do you want to continue?
-read -p "Do you want to continue? (y/n): " choice
+clear -x
 
-if [ $choice == "y" ]; then
-    echo "Continuing with the installation"
+# First install homebrew 
+echo "Installing Homebrew... "
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
+
+clear -x 
+
+echo "Setting environment variables..."
+# Set environment variables
+
+# Check if brew is in /usr/local/bin/ or /opt/homebrew/bin 
+# and set the shellenv accordingly
+# as well as add the shellenv to the shell profile
+
+if [ -f /usr/local/bin/brew ]; then
+    echo "Brew is installed in /usr/local/bin"
+    (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zprofile
+    (echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.bash_profile
+    eval "$(/usr/local/bin/brew shellenv)"
+elif [ -f /opt/homebrew/bin/brew ]; then
+    echo "Brew is installed in /opt/homebrew/bin"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.bash_profile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-    echo "Exiting the installation"
-    exit 1
+    echo "Brew is not installed correctly. Exiting"
+    exit_message
 fi
 
 clear -x
 
-# First install homebrew 
-echo "Installing Homebrew. Please following the intructions on the screen"
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Set environment variables
-
-
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.zshrc
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> ~/.bash_profile
-eval "$(/usr/local/bin/brew shellenv)"
-eval "$(/opt/homebrew/bin/brew shellenv)"
 # update terminal 
 hash -r 
 
@@ -47,53 +60,88 @@ if brew help > /dev/null; then
 else
     echo "Homebrew installation failed. Exiting"
     exit_message
-    exit 1
 fi
 
 # Install miniconda
 # Check if miniconda is installed
 
-echo "Installing Miniconda"
+echo "Installing Miniconda..."
 if conda --version > /dev/null; then
     echo "Miniconda or anaconda is already installed"
 else
     echo "Installing Miniconda"
     brew install --cask miniconda 
+    if [ $? -ne 0 ]; then
+        exit_message
+    fi
 fi
+clear -x
 
+
+echo "Initialising conda..."
 # Finally downgrade python version of base environment to 3.11
 conda init bash 
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
 conda init zsh
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
 # need to restart terminal to activate conda
 # restart terminal and continue
-
 
 # 'restart' terminal
 eval "$(/usr/local/bin/brew shellenv)"
 
 hash -r 
+clear -x
 # Install anaconda GUI
 echo "Installing Anaconda Navigator GUI"
 conda install anaconda-navigator -y
+if [ $? -ne 0 ]; then
+    exit_message
+fi
 
+echo "Downgrading python version to 3.11..."
 conda install python=3.11 -y
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+clear -x 
 
+echo "Installing packages..."
 conda install -c conda-forge dtumathtools uncertainties -y
-
-
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+clear -x
 
 # check if vs code is installed
 # using multipleVersionsMac to check 
-echo "Installing Visual Studio Code if not already installed"
+echo "Installing Visual Studio Code if not already installed..."
 # if output is empty, then install vs code
 vspath=$(/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/dtudk/pythonsupport-scripts/main/multipleVersionsMac.sh)")
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
 if [ -n "$vspath" ]  ; then
     echo "Visual Studio Code is already installed"
 else
     echo "Installing Visual Studio Code"
     brew install --cask visual-studio-code
+    if [ $? -ne 0 ]; then
+        exit_message
+    fi
 fi
+
 hash -r 
+clear -x
+
+
 echo "Installing extensions for Visual Studio Code"
 eval "$(/usr/local/bin/brew shellenv)"
 
@@ -103,20 +151,30 @@ if code --version > /dev/null; then
 else
     echo "Visual Studio Code installation failed. Exiting"
     exit_message
-    exit 1
 fi
+clear -x
 
+echo "Installing extensions for Visual Studio Code..."
 # install extensions for vs code
 # install python extension, jupyter, vscode-pdf
 #python extension
 code --install-extension ms-python.python
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
 #jupyter extension
 code --install-extension ms-toolsai.jupyter
+if [ $? -ne 0 ]; then
+    exit_message
+fi
+
 #pdf extension (for viewing pdfs inside vs code)
 code --install-extension tomoki1207.pdf
+if [ $? -ne 0 ]; then
+    exit_message
+fi
 
 hash -r 
 
-echo "Script has finished"
-
-
+echo "Script has finished. You may now close the terminal"
