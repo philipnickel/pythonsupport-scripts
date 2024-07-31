a31f4186cedf44ac320906c1c73e5f54ec277719
philipnickel <tipkik@hotmail.com>
2024-07-31 10:34:45 +0200

    fixes to windows

--- AutoInstallWindows.ps1
+++ AutoInstallWindows.ps1
@@ -36,126 +36,129 @@ $minicondaPath2 = "C:\ProgramData\Miniconda3"
 $anacondaPath1 = "$env:USERPROFILE\Anaconda3"
 $anacondaPath2 = "C:\ProgramData\Anaconda3"
 
-if (Test-Path $minicondaPath1 -or Test-Path $minicondaPath2 -or Test-Path $anacondaPath1 -or Test-Path $anacondaPath2) {
+if ((Test-Path $minicondaPath1) -or (Test-Path $minicondaPath2) -or (Test-Path $anacondaPath1) -or (Test-Path $anacondaPath2)) {
     Write-Host "Miniconda or Anaconda is already installed. Skipping Miniconda installation."
     Write-Host "If you wish to install Miniconda using this script, please uninstall the existing Anaconda/Miniconda installation and run the script again."
-    goto vscode_installation
-}
-
-# Script by Python Installation Support DTU
-Write-Host "This script will install Python along with Visual Studio Code - and everything you need to get started with programming"
-Write-Host "This script will take a while to run, please be patient, and don't close PowerShell before it says 'script finished'."
-Start-Sleep -Seconds 3
-
-# Download the Miniconda installer
-$minicondaUrl = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
-$minicondaInstallerPath = "$env:USERPROFILE\Downloads\Miniconda3-latest-Windows-x86_64.exe"
-
-Write-Host "Downloading installer for Miniconda..."
-Invoke-WebRequest -Uri $minicondaUrl -OutFile $minicondaInstallerPath
-if ($?) {
-    Write-Host "Miniconda installer downloaded."
 } else {
-    Exit-Message
-}
+    # Script by Python Installation Support DTU
+    Write-Host "This script will install Python along with Visual Studio Code - and everything you need to get started with programming"
+    Write-Host "This script will take a while to run, please be patient, and don't close PowerShell before it says 'script finished'."
+    Start-Sleep -Seconds 3
 
-Write-Host "Will now install Miniconda..."
-# Install Miniconda
-Start-Process -FilePath $minicondaInstallerPath -ArgumentList "/InstallationType=JustMe /RegisterPython=1 /S /D=$env:USERPROFILE\Miniconda3" -Wait
-if ($?) {
-    Write-Host "Miniconda installed."
-} else {
-    Exit-Message
-}
+    # Download the Miniconda installer
+    $minicondaUrl = "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+    $minicondaInstallerPath = "$env:USERPROFILE\Downloads\Miniconda3-latest-Windows-x86_64.exe"
 
-# Add Anaconda to PATH and refresh environment variables
-function Add-CondaToPath {
-    if (Test-Path "$env:USERPROFILE\Miniconda3\condabin") {
-        $condaPath = "$env:USERPROFILE\Miniconda3\condabin"
-    } elseif (Test-Path "C:\ProgramData\Miniconda3\condabin") {
-        $condaPath = "C:\ProgramData\Miniconda3\condabin"
+    Write-Host "Downloading installer for Miniconda..."
+    Invoke-WebRequest -Uri $minicondaUrl -OutFile $minicondaInstallerPath
+    if ($?) {
+        Write-Host "Miniconda installer downloaded."
     } else {
-        Write-Host "Miniconda is not installed."
         Exit-Message
     }
 
-    if (-not ($env:Path -contains $condaPath)) {
-        [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$condaPath", [System.EnvironmentVariableTarget]::User)
+    Write-Host "Will now install Miniconda..."
+    # Install Miniconda
+    Start-Process -FilePath $minicondaInstallerPath -ArgumentList "/InstallationType=JustMe /RegisterPython=1 /S /D=$env:USERPROFILE\Miniconda3" -Wait
+    if ($?) {
+        Write-Host "Miniconda installed."
+    } else {
+        Exit-Message
+    }
+
+    # Add Anaconda to PATH and refresh environment variables
+    function Add-CondaToPath {
+        if (Test-Path "$env:USERPROFILE\Miniconda3\condabin") {
+            $condaPath = "$env:USERPROFILE\Miniconda3\condabin"
+        } elseif (Test-Path "C:\ProgramData\Miniconda3\condabin") {
+            $condaPath = "C:\ProgramData\Miniconda3\condabin"
+        } else {
+            Write-Host "Miniconda is not installed."
+            Exit-Message
+        }
+
+        if (-not ($env:Path -contains $condaPath)) {
+            [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$condaPath", [System.EnvironmentVariableTarget]::User)
+        }
+    }
+
+    Add-CondaToPath
+    if ($?) {
+        Refresh-Env
+        Write-Host "Environment variables refreshed."
+    } else {
+        Exit-Message
+    }
+
+    # Re-import the updated PATH for the current session
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+
+    # Activate conda base environment
+    & "$env:USERPROFILE\Miniconda3\condabin\conda.bat" activate
+    if ($?) {
+        Write-Host "Conda base environment activated."
+    } else {
+        Exit-Message
+    }
+
+    # Initialize conda
+    & "$env:USERPROFILE\Miniconda3\condabin\conda.bat" init
+    if ($?) {
+        Write-Host "Conda initialized."
+    } else {
+        Exit-Message
+    }
+
+    # Ensure version of Python
+    & "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install python=3.11 -y
+    if ($?) {
+        Write-Host "Python 3.11 installed."
+    } else {
+        Exit-Message
+    }
+
+    # Install the GUI (Anaconda Navigator)
+    & "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install anaconda-navigator -y
+    if ($?) {
+        Write-Host "Anaconda Navigator installed."
+    } else {
+        Exit-Message
+    }
+
+    # Install packages
+    & "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install -c conda-forge dtumathtools uncertainties -y
+    if ($?) {
+        Write-Host "Additional packages installed."
+    } else {
+        Exit-Message
     }
 }
 
-Add-CondaToPath
-if ($?) {
-    Refresh-Env
-    Write-Host "Environment variables refreshed."
+# Check if VS Code is already installed
+$vscodePath = "C:\Users\$env:USERNAME\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+if (Test-Path $vscodePath) {
+    Write-Host "Visual Studio Code is already installed. Skipping VS Code installation."
 } else {
-    Exit-Message
-}
+    # Download the VS Code installer
+    $vscodeUrl = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
+    $vscodeInstallerPath = "$env:USERPROFILE\Downloads\vscode-installer.exe"
 
-# Re-import the updated PATH for the current session
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+    Write-Host "Downloading installer for Visual Studio Code..."
+    Invoke-WebRequest -Uri $vscodeUrl -OutFile $vscodeInstallerPath
+    if ($?) {
+        Write-Host "VS Code installer downloaded."
+    } else {
+        Exit-Message
+    }
 
-# Activate conda base environment
-& "$env:USERPROFILE\Miniconda3\condabin\conda.bat" activate
-if ($?) {
-    Write-Host "Conda base environment activated."
-} else {
-    Exit-Message
-}
-
-# Initialize conda
-& "$env:USERPROFILE\Miniconda3\condabin\conda.bat" init
-if ($?) {
-    Write-Host "Conda initialized."
-} else {
-    Exit-Message
-}
-
-# Ensure version of Python
-& "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install python=3.11 -y
-if ($?) {
-    Write-Host "Python 3.11 installed."
-} else {
-    Exit-Message
-}
-
-# Install the GUI (Anaconda Navigator)
-& "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install anaconda-navigator -y
-if ($?) {
-    Write-Host "Anaconda Navigator installed."
-} else {
-    Exit-Message
-}
-
-# Install packages
-& "$env:USERPROFILE\Miniconda3\condabin\conda.bat" install -c conda-forge dtumathtools uncertainties -y
-if ($?) {
-    Write-Host "Additional packages installed."
-} else {
-    Exit-Message
-}
-
-:vscode_installation
-
-# Download the VS Code installer
-$vscodeUrl = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
-$vscodeInstallerPath = "$env:USERPROFILE\Downloads\vscode-installer.exe"
-
-Write-Host "Downloading installer for Visual Studio Code..."
-Invoke-WebRequest -Uri $vscodeUrl -OutFile $vscodeInstallerPath
-if ($?) {
-    Write-Host "VS Code installer downloaded."
-} else {
-    Exit-Message
-}
-
-Write-Host "Installing Visual Studio Code..."
-# Install VS Code
-Start-Process -FilePath $vscodeInstallerPath -ArgumentList "/verysilent /norestart /mergetasks=!runcode" -Wait
-if ($?) {
-    Write-Host "VS Code installed."
-} else {
-    Exit-Message
+    Write-Host "Installing Visual Studio Code..."
+    # Install VS Code
+    Start-Process -FilePath $vscodeInstallerPath -ArgumentList "/verysilent /norestart /mergetasks=!runcode" -Wait
+    if ($?) {
+        Write-Host "VS Code installed."
+    } else {
+        Exit-Message
+    }
 }
 
 # Refresh environment variables
