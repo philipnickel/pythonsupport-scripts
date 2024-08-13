3678e03ec7231e7254dda23f69ad0ab821b8cb6c
Nick Papior <nickpapior@gmail.com>
2024-08-13 12:51:48 +0200

    cleaned scripts all over, ensured correct URL path was used
    This meant that the REMOTE_PS had to have the full <user>/<repo>
    to be self-contained.
    
    Since this is a dev-tool, it should not be a problem.
    
    Only thing is that windows scripts still needs to be functional.
    
    Signed-off-by: Nick Papior <nickpapior@gmail.com>

--- MacOSAuto_python.sh
+++ MacOSAuto_python.sh
@@ -1,8 +1,22 @@
+# Get URL
+if [ -z "$REMOTE_PS" ]; then
+  REMOTE_PS="dtudk/pythonsupport-scripts"
+fi
+if [ -z "$BRANCH_PS" ]; then
+  BRANCH_PS="main"
+fi
+
+export REMOTE_PS
+export BRANCH_PS
+
+# set URL 
+url_ps="https://raw.githubusercontent.com/$REMOTE_PS/$BRANCH_PS"
+
 # Check for homebrew
 # if not installed call homebrew installation script
 if ! command -v brew > /dev/null; then
   echo "Homebrew is not installed. Installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL $path_ps/MacOSAuto_Homebrew.sh)"
+  /bin/bash -c "$(curl -fsSL $url_ps/MacOSAuto_Homebrew.sh)"
 fi
 
 
